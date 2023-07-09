7b80cba707b9efc7cc6be8a4c845bdeba449ba15
VadymSchastlyvtsev <drdre6870@gmail.com>
2023-07-09 13:11:52 +0300

    lesoon 1
    addad  marcup to first color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+let validBtn;
+
+startBtn.addEventListener('click', handlerStart);
+
+stopBtn.addEventListener('click', handlerStop);
+
+ function handlerStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+  };
+
+function handlerStop() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(intervalId);
+  };
+
+
+
+
 
